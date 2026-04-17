5fdb8c2e5f7427fd2aacfa340544ccd0e772481d
Jhon057 <jhon01rodriguez@gmail.com>
2026-04-16 20:44:37 -0500

    modelo paciente y repositorio

--- src/main/java/com/devSenior/hackaton/model/Paciente.java
+++ src/main/java/com/devSenior/hackaton/model/Paciente.java
@@ -1,5 +1,51 @@
 package com.devSenior.hackaton.model;
 
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Paciente {
 
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 20, message = "El nombre debe tener entre 3 y 20 caracteres")
+    private String nombre;
+
+    @NotBlank
+    @Size(min = 3, max = 20, message = "El apellido debe tener entre 3 y 20 caracteres")
+    private String apellido;
+
+    @Positive
+    private int edad;
+
+    @NotBlank
+    private String genero;
+    @NotBlank
+    @Size(min = 7, max = 10, message = "El número debe estar entre 7 y 10 caracteres")
+    private String telefono;
+
+    @NotBlank
+    @Size(min = 3, max = 20, message = "El apellido debe tener entre 3 y 20 caracteres")
+    private String direccion;
+
+    private LocalDate fechaNacimiento;
+
+    @NotBlank
+    @Size(min = 3, max = 20, message = "El apellido debe tener entre 3 y 20 caracteres")
+    private String historialMedico;
+
+    @NotBlank
+    @Size(min = 3, max = 20, message = "El apellido debe tener entre 3 y 20 caracteres")
+    private String medicoAsignado;
 }
